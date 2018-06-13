8f976e02518d7c1326e03c64fd463f5f8a41d762
vschaitu <vschaitu@gmail.com>
2018-06-13 15:33:05 -0500

    Add more friends to data array

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,116 +1,132 @@
 var friends = [
     {
-    "name": "Ahmed",
-    "photo": "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-0.3.5&s=f7007b5e33474e6a9a02403d0db403d9&auto=format&fit=crop&w=2250&q=80",
-    "scores": [
-    "5",
-    "1",
-    "4",
-    "4",
-    "5",
-    "1",
-    "2",
-    "5",
-    "4",
-    "1"
-    ]
+        "name": "Ahmed",
+        "photo": "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-0.3.5&s=f7007b5e33474e6a9a02403d0db403d9&auto=format&fit=crop&w=2250&q=80",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
     },
     {
-    "name": "Jacob Deming",
-    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-    "scores": [
-    "4",
-    "2",
-    "5",
-    "1",
-    "3",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2"
-    ]
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
     },
     {
-    "name": "Jeremiah Scanlon",
-    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-    "scores": [
-    "5",
-    "2",
-    "2",
-    "2",
-    "4",
-    "1",
-    "3",
-    "2",
-    "5",
-    "5"
-    ]
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
     },
     {
-    "name": "Louis T. Delia",
-    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    "scores": [
-    "3",
-    "3",
-    "4",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2",
-    "2",
-    "3"
-    ]
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
     },
     {
-    "name": "Lou Ritter",
-    "photo": "https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6138d4bb4d3515092f3379fb64fbbb00&auto=format&fit=crop&w=2250&q=80",
-    "scores": [
-    "4",
-    "3",
-    "4",
-    "1",
-    "5",
-    "2",
-    "5",
-    "3",
-    "1",
-    "4"
-    ]
+        "name": "Lou Ritter",
+        "photo": "https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6138d4bb4d3515092f3379fb64fbbb00&auto=format&fit=crop&w=2250&q=80",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
     },
     {
-    "name": "Jordan Biason",
-    "photo": "https://images.unsplash.com/photo-1440589473619-3cde28941638?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42ebdb92a644e864e032a2ebccaa25b6&auto=format&fit=crop&w=934&q=80",
-    "scores": [
-    "4",
-    "4",
-    "2",
-    "3",
-    "2",
-    "2",
-    "3",
-    "2",
-    "4",
-    "5"
-    ]
+        "name": "Jordan Biason",
+        "photo": "https://images.unsplash.com/photo-1440589473619-3cde28941638?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42ebdb92a644e864e032a2ebccaa25b6&auto=format&fit=crop&w=934&q=80",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
     },
     {
         "name": "Chaitu",
         "photo": "https://images.unsplash.com/photo-1458724338480-79bc7a8352e4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7b54da7a4d4a137d0272c5de7ed68d54&auto=format&fit=crop&w=2250&q=80",
         "scores": [
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3"
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
         ]
-        }
+    },
+    {
+        "name": "lorem ipsum",
+        "photo": "https://www.fillmurray.com/640/360",
+        "scores": [
+            "4",
+            "4",
+            "4",
+            "3",
+            "2",
+            "3",
+            "1",
+            "1",
+            "1",
+            "3"
+        ]
+    }
 ];
 
 module.exports = friends;
